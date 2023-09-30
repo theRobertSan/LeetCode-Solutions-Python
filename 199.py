caae5fcb18bcdadbb89b4fa139359590d2a46aaa
theRobertSan <robertsan.pro@gmail.com>
2023-09-30 23:21:37 +0200

    Add solution to problem 199

---
+++ 199.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+            result.append(queue[-1].val)
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
