0681b9180168ed8edf2c14b309f9ecbe00333ca4
theRobertSan <robertsan.pro@gmail.com>
2023-11-28 20:14:53 +0000

    Add solution to problem 462

---
+++ 462.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        median = nums[len(nums) // 2]
+
+        moves = 0
+        for num in nums:
+            moves += abs(num - median)
+
+        return moves
